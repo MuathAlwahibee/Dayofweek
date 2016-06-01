87b4f292bfdaa16eab1780803cdf194211da3c4e
MuathAlwahibee <mo3ath.20@hotmail.com>
2016-06-01 13:17:20 -0400

    Add files via upload

---
+++ DistanceMuath Alwahibee.py
@@ -0,0 +1,13 @@
+print("CSC 122-W1 - Program #1");
+print("By <Student Muath Alwahibee> \n<due May 26>")
+
+#Get the speed 
+speed = int(input("\nWhat is the speed of the vehicale in mph? "))
+
+#Get the number of hours
+time = int(input("How many hours did you drive? "))
+
+print("{:>0}{:>10}". format("Hour", "Distance"))
+
+for hour in range(1, time+1):
+    print("{:>0}{:>10}". format(hour, hour * speed))
