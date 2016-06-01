b13b62db1bee955f4f22ac1faaa5b1c6b57249af
MuathAlwahibee <mo3ath.20@hotmail.com>
2016-06-01 13:26:53 -0400

    Update GroceryProgram.py

--- GroceryProgram.py
+++ GroceryProgram.py
@@ -1,6 +1,7 @@
 print("CSC 122-W1 - Program #1");
 print("By <Student Muath Alwahibee> \n<due May 26>")
 
+#We use the float statemnt to write a number with a decimal
 item1 = float(input("\nEnter thr price of item #1: "))
 item2 = float(input("Enter the price of item #2: "))
 item3 = float(input("Enter the price of item #3: "))
@@ -12,6 +13,7 @@ subtotal = a
 sales_tax = a * 0.07
 total = a * 0.07 + a
 
+#We use format statemnt with ".2f" to have 2 digits only to the write of the decimal.
 print ("\nSubtotal:",format(subtotal, '.2f'))
 print ("Sales Tax:", format(sales_tax, '.2f'))
 
